9c5cc52346b81a0be284f0e4a41f29f978828816
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-12-09 15:36:35 +0700

    add class program, define data type needed

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
+namespace Exercise_4
+{
+    class Program
+    {
+        //Declare array
+        char[] array = new char[18];
+
+        int top = -1;
+
+        
+    }
+}
+
+
+
+// Data types char
+// max number = rata2(9 + 90 + 1 + 2 x 10 - 10)
+//            = 100 + 20 - 10
+//            = 110/6 = 18
+//array = wildan
